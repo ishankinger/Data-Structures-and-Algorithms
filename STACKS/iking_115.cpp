bd38ee441ba93ab47f6ab4de0537c06fc92a9554
ishan420 <116195006+ishankinger@users.noreply.github.com>
2023-10-27 09:47:55 +0530

    Update iking_115.cpp

--- STACKS/iking_115.cpp
+++ STACKS/iking_115.cpp
@@ -1,17 +1,22 @@
-// CONVERSION OF A PREFIX EXPRESSION TO A POSTFIX EXPRESSION AND VICE VERSA
+// ************************ CONVERSIONS OF PREFIX EXPRESSIONS AND POSTFIX EXPRESSIONS **************************
 // PREFIX EXPRESSION ==> * + 3 2 - 1 5
 // POSTFIX EXPRESSION ==> 3 2 + 1 5 - *
 
 #include<bits/stdc++.h>
 using namespace std;
 
-// PREFIX TO POSTFIX
+// *************** PREFIX TO POSTFIX *********************
 
-string preToPost(string & pre){
+string prefix_To_Postfix(string & pre){
+    // AN EMPTY STACK OF STRING DATATYPE IS CREATED
     stack<string>st;
+    // REVERSE THE STRING
     reverse(pre.begin(),pre.end());
+    // GOING IN A LOOP FROM LEFT TO RIGHT
     for(int i=0;i<pre.size();i++){
+        // IF CURRENT CHAR IS DIGIT THEN PUSH INTO THE STACK
         if(isdigit(pre[i])) st.push(to_string(pre[i]-'0'));
+        // ELSE IF IT IS OPERATOR THEN CONVERT TOP TWO ELEMENTS AND OPERATOR INTO POSTFIX EXPRESSION AND INSERT INTO THE STACK AS A STRING
         else{
             string v1=st.top();
             st.pop();
@@ -21,15 +26,20 @@ string preToPost(string & pre){
             st.push(newexp);
         }
     }
+    // AT END ONLY ONE STRING WILL LEFT WHICH WILL BE OUR FINAL ANSWER EXPRESSION
     return st.top();
 }
 
-// POSTFIX TO PREFIX
+// ****************** POSTFIX TO PREFIX *************************
 
-string postToPre(string &post){
-    stack<string> st;
+string postfix_To_Prefix(string &post){
+    // AN EMPTY STACK OF STRING DATATYPE IS CREATED
+    stack<string>st;
+    // GOING IN A LOOP FROM LEFT TO RIGHT
     for(int i=0;i<post.size();i++){
+        // IF CURRENT CHAR IS DIGIT THEN PUSH INTO THE STACK
         if(isdigit(post[i])) st.push(to_string(post[i]-'0'));
+        // ELSE IF IT IS OPERATOR THEN CONVERT TOP TWO ELEMENTS AND OPERATOR INTO PREFIX EXPRESSION AND INSERT INTO THE STACK AS A STRING
         else{
             string v1 = st.top();
             st.pop();
@@ -39,13 +49,14 @@ string postToPre(string &post){
             st.push(newExp);
         }
     }
+    // AT END ONLY ONE STRING WILL LEFT WHICH WILL BE OUR FINAL ANSWER EXPRESSION
     return st.top();
 }
 
 int main(){
     string s="*+32-15";
-    cout<<preToPost(s)<<"\n";
-    cout<<postToPre(s);
+    cout<<prefix_To_Postfix(s)<<"\n";
+    cout<<postfix_To_Prefix(s)<<"\n";
 
     return 0;
 }
