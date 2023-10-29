8d2accda72a9f2251577320730da2354d388e03e
ishan420 <116195006+ishankinger@users.noreply.github.com>
2023-10-29 14:34:14 +0530

    Update iking_103.cpp

--- SORTING TECHNIQUES/iking_103.cpp
+++ SORTING TECHNIQUES/iking_103.cpp
@@ -1,4 +1,15 @@
-// INSERTION SORT
+// ****************** INSERTION SORT *********************
+
+// In this algorithm, each iteration removes an element from the input data and 
+// inserts it into the correct position in the list being sorted. 
+
+// >> Adaptive: If the input list is presorted [may not be completely] then insertions sort
+//              takes O(n + d), where d is the number of inversions
+// >> Stable: Maintains relative order of input data if the keys are same
+
+// Insertion sort is one of the elementary sorting algorithms with O(n2) worst-case time. Insertion
+// sort is used when the data is nearly sorted (due to its adaptiveness) or when the input size is small
+// (due to its low overhead).
 
 // FIRST TRAVERSE ON EACH ELEEMENT AND  INNER WHILE LOOP WILL TRAVERSE BACK TO THAT ELEMENT
 // WHILE TRAVERSING BACK IF WE GET ELEMENT LESSER THAN IT THEN INSERT OUR ELEMENT NEXT TO IT 
@@ -8,15 +19,18 @@
 using namespace std;
 
 int main(){
-    int arr[]={12,45,51,23,8,19};          /* FOR 23 FIRST WE WILL TRAVERSE BACK AND FIRST 51 OCCURS NOW 51 IS GREATER SO ON 23 POSITION
-//                                            51 WILL TAKE PLACE AND FURTHER MOVING 45 IS ALSO GREATER SO OLD 51 POSITION TAKEN BY 45 
-//                                            NOW 12 IS LESSER SO LOOP WILL BREAK AND
-//                                            23 WILL BE INSERTED NEXT TO 12 */
+    
+    int arr[]={12,45,51,23,8,19}; 
+    // FOR 23 FIRST WE WILL TRAVERSE BACK AND FIRST 51 OCCURS NOW 51 IS GREATER SO ON 23 POSITION
+    // 51 WILL TAKE PLACE AND FURTHER MOVING 45 IS ALSO GREATER SO OLD 51 POSITION TAKEN BY 45 
+    // NOW 12 IS LESSER SO LOOP WILL BREAK AND
+    // 23 WILL BE INSERTED NEXT TO 12 
+    
     for(int i=1;i<6;i++){
         int currEle=arr[i];                // TAKE REFERENCE OF ITH ELEMENT
         int j=i-1;                         // START TRAVERSING FROM PREVIOUS OF ITH ELEMENT
-        while(arr[j]>currEle && j>=0){     // IF WE GET ELEMENT GREATER THAN IT THEN GREATER ELEMENT WILL BE PUSHED IN JTH
-            arr[j+1]=arr[j];               
+        while(j >= 0 && arr[j]>currEle){   // IF WE GET ELEMENT GREATER THAN IT THEN GREATER ELEMENT WILL BE PUSHED IN JTH
+            arr[j+1]=arr[j];                     
             j--;                           // FURTHER MOVE BACK
         }
         arr[j+1]=currEle;                  // WHEN WE GET OUT OF LOOP MEANS WE REACH OUR REQUIRED CONDITION
@@ -29,4 +43,3 @@ int main(){
 
 // >>> TIME COMPLEXITY = O(N2)
 // >>> SPACE COMPLEXITY = O(1)
-// >>> STABLE SORTING ALGORITHM
