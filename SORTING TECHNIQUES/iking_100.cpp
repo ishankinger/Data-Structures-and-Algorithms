e26d08726ab96aba657f0f05664303da8bf02196
ishan420 <116195006+ishankinger@users.noreply.github.com>
2023-10-29 14:15:27 +0530

    Create iking_100.cpp

---
+++ SORTING TECHNIQUES/iking_100.cpp
@@ -0,0 +1,25 @@
+// Sorting is an algorithm that arranges the elements of a list in a certain order [either ascending or
+// descending]. The output is a permutation or reordering of the input.
+
+// -> By no. of Comparisons : compare via no. of comparisons taking place in algo.
+// /                           best case = O(nlog(n)) and worst case = O(n2).
+
+// -> By no. of swaps : comparison via no. of swaps(also called inversions).
+
+// -> By memory usage : Sorting is an algorithm that arranges the elements of a list in a certain order [either ascending or
+// /                     descending]. The output is a permutation or reordering of the input. 
+
+// -> By recursion : Sorting algorithms are either recursive [quick sort] or non-recursive [selection sort, and insertion
+// /                  sort], and there are some algorithms which use both (merge sort).
+
+// -> By stability : Few sorting algorithms maintain the relative order of elements with equal keys (equivalent
+// /                 elements retain their relative positions even after sorting).
+
+// -> By adaptability : With a few sorting algorithms, the complexity changes based on pre-sortedness [quick sort]:
+// /                   presortedness of the input affects the running time. Algorithms that take this into account are known to be adaptive.
+
+// -> Internal Sort : Sort algorithms that use main memory exclusively during the sort are called internal sorting
+// /                  algorithms. This kind of algorithm assumes high-speed random access to all memory.
+
+// -> External Sort : Sorting algorithms that use external memory, such as tape or disk, during the sort come under this
+// /                  category.
