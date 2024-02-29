010385d52e2a10fb0c624b44735ebb6341578caf
ishan420 <116195006+ishankinger@users.noreply.github.com>
2024-02-29 08:01:55 +0530

    Update iking_118.cpp

--- GRAPHS/iking_118.cpp
+++ GRAPHS/iking_118.cpp
@@ -1,39 +1,47 @@
 // DETECT A CYCLE IN A DIRECTED GRAPH USING DFS 
+// SIMILAR CONCEPT USED AS THAT OF USED IN BFS TRAVERSAL ONLY TECHNIQUE IS DIFFERENT
 
 #include<bits/stdc++.h>
 using namespace std;
 
-bool dfs(int src, vector< list<int> > graph, vector<int> &vis, vector<int> &path_vis){
-    vis[src] = 1;
-    path_vis[src] = 1;
+bool dfs(int src, vector<int> &color, vector< list<int> > graph, int col){
+    
+    // first change the color to not of it's parent one
+    color[src] = not(col);
+
+    // traverse to all the neighbours of our curr node
     for(auto neighbour : graph[src]){
-        if(not vis[neighbour]){
-            if(dfs(neighbour,graph,vis,path_vis)){
-                return true;
-            }
-            else{
-                path_vis[neighbour] = 0;
-            }
+
+        // if neighbour is not visited 
+        if(color[neighbour] == -1){
+
+            // call for it's neighbour and if we get false from this call then return false
+            if(not dfs(neighbour,color,graph,color[src]))
+                return false;  
         }
+
+        // else if neighbour is already visited
         else{
-            if(path_vis[neighbour]){
-                return true;
+
+            // if they have same color then return false
+            if(color[neighbour] == color[src]){
+                return false;
             }
         }
     }
-    return false;
+    return true;
 }
 
-bool isCycle(vector< list<int> > graph){
-    vector<int> vis(graph.size(),0);
-    vector<int> path_vis(graph.size(),0);
+bool isBipartite(vector< list<int> > graph){
+    vector<int> color(graph.size(),-1);
+    // traverse in all the nodes
     for(int i = 0; i < graph.size(); i++){
-        if(not vis[i]){
-            bool check = dfs(i,graph,vis,path_vis);
-            if(check) return true;
+        if(color[i] == -1){
+            bool check = dfs(i,color,graph,1);
+            if(not check) return false;
         }
     }
-    return false;
+    return true;
 }
 
 int main(){
@@ -47,6 +55,7 @@ int main(){
         int u, v;
         cin >> u >> v;
         graph[u].push_back(v);
+        graph[v].push_back(u);
     }
 
     for(int ele = 0; ele < graph.size(); ele++){
@@ -57,11 +66,11 @@ int main(){
         cout << "\n";
     }
 
-    if(isCycle(graph)){
-        cout<<"The graph has a cycle\n";
+    if(isBipartite(graph)){
+        cout << "The graph is Bipartite\n";
     }
     else{
-        cout<<"The graph doesnot have a cycle\n";
+        cout << "The graph is not Bipartite\n";
     }
 
     return 0;
