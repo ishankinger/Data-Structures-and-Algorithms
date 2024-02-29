c392b7bbb81f717688c114164d74ffd27e534347
ishan420 <116195006+ishankinger@users.noreply.github.com>
2024-02-29 08:03:47 +0530

    Update iking_117.cpp

--- GRAPHS/iking_117.cpp
+++ GRAPHS/iking_117.cpp
@@ -1,16 +1,29 @@
 // CHECK WHETHER A GRAPH IS BIPARTITE OR NOT USING DFS
+// SIMILAR CONCEPT USED AS THAT OF USED IN BFS TRAVERSAL ONLY TECHNIQUE IS DIFFERENT
 
 #include<bits/stdc++.h>
 using namespace std;
 
 bool dfs(int src, vector<int> &color, vector< list<int> > graph, int col){
+    
+    // first change the color to not of it's parent one
     color[src] = not(col);
+
+    // traverse to all the neighbours of our curr node
     for(auto neighbour : graph[src]){
+
+        // if neighbour is not visited 
         if(color[neighbour] == -1){
+
+            // call for it's neighbour and if we get false from this call then return false
             if(not dfs(neighbour,color,graph,color[src]))
                 return false;  
         }
+
+        // else if neighbour is already visited
         else{
+
+            // if they have same color then return false
             if(color[neighbour] == color[src]){
                 return false;
             }
@@ -21,6 +34,7 @@ bool dfs(int src, vector<int> &color, vector< list<int> > graph, int col){
 
 bool isBipartite(vector< list<int> > graph){
     vector<int> color(graph.size(),-1);
+    // traverse in all the nodes
     for(int i = 0; i < graph.size(); i++){
         if(color[i] == -1){
             bool check = dfs(i,color,graph,1);
