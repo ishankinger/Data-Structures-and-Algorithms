be5aa322d904d9a519b34a2ead3832700a822474
ishan420 <116195006+ishankinger@users.noreply.github.com>
2024-03-01 11:59:12 +0530

    Update iking_119.cpp

--- GRAPHS/iking_119.cpp
+++ GRAPHS/iking_119.cpp
@@ -1,25 +1,46 @@
-// ?????????????? FIND EVENTUAL SAFE NODES
+// FIND EVENTUAL SAFE NODES
+// A SAFE NODE IS A NODE HAVING ALL IT'S PATH ENDING AT A TERMINAL NODE ( NODE WHICH HAVE 0 OUTGOING EDGES )
+// NODES WHICH FORM A CYCLE CAN'T BE A SAFE NODE
+// ALSO NODES WHICH ARE ENTERING INTO THE CYCLE WILL ALSO NOT BE A SAFE NODE
+// EXCEPT THAT ALL NODES WILL BE SAFE NODES
 
 #include<bits/stdc++.h>
 using namespace std;
 
-bool dfs(int src, vector< list<int> > graph, vector<int> &vis,
-         vector<int> &path_vis, vector<int> &safe){
+bool dfs(int src, vector< list<int> > graph, vector<int> &vis, vector<int> &path_vis, vector<int> &safe){
+    // make curr node as visited, path_visited and not a safe node
     vis[src] = 1;
     path_vis[src] = 1;
     safe[src] = 0;
+
+    // traverse on all the neighbours of source
     for(auto neighbour : graph[src]){
+
+        // if neighbour is not visited
         if(not vis[neighbour]){
+
+            // then run dfs on neighbour and if give true means cycle present
             if(dfs(neighbour,graph,vis,path_vis,safe)){
+
+                // then this node is in the cycle so can't be a safe node
                 safe[src] = 0;
+
+                // and return true
                 return true;
             }
         }
+
+        // else is not visited and is path visited
         else if(path_vis[neighbour]){
+            // so this node is in cycle
+            // so not safe and return true
             safe[src] = 0;
             return true;
         }
     }
+
+    // else if neighbour doesnot result in cycle formation
+    // then it is a safe node and make path visited = 0 and return false
     path_vis[src] = 0;
     safe[src] = 1;
     return false;
