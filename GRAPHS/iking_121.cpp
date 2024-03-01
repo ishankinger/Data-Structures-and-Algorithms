abbd5aedf3e1e1ba5f1a250b057601f09f9be634
ishan420 <116195006+ishankinger@users.noreply.github.com>
2024-03-01 13:14:34 +0530

    Update iking_121.cpp

--- GRAPHS/iking_121.cpp
+++ GRAPHS/iking_121.cpp
@@ -1,34 +1,44 @@
 // KAHN'S ALGORITHM ( TOPOLOGICAL SORT USING BFS )
-// KAHN'S ALGO IS VALID FOR DAG ONLY BUT IF IT'S CYCLIC THEN IT WILL NOT GIVE FULL SEQUENCE
+// ELEMENT OF INDEGREE 0 IS PUSHED INTO THE QUEUE AND WHEN WE ARE POPPING OUT THE ELEMENT ALL IT'S NEIGHBOUR EDGES ARE ALSO TO BE REMOVED
 
 #include<bits/stdc++.h>
 using namespace std;
 
-// applying same code of kahn's algorithm
-int isCycle(vector< vector<int> > graph){
-    vector<int> topo;
+void topologicalSort(vector< vector<int> > graph){
+
+    // indegree array for all the nodes is maintained
     vector<int> indegree(graph.size(),0);
     for(int i = 0; i < graph.size(); i++){
         for(int j = 0; j < graph[i].size(); j++){
             indegree[graph[i][j]]++;
         }
     }
+
+    // first we will push all elements of indegree 0 in our queue
     queue<int> q;
-    for(int i = 0; i < indegree.size(); i++){
+    for(int i = 0; i < graph.size(); i++){
         if(indegree[i] == 0){
             q.push(i);
         }
     }
+
+    // traverse till the queue is not empty
     while(not q.empty()){
+        
+        // taking the front element of the queue
         int ele = q.front();
+        cout<<ele<<" ";
         q.pop();
-        topo.push_back(ele);
+
+        // for each node it's neighbour's will also be removed
         for(auto neighbour : graph[ele]){
             indegree[neighbour]--;
-            if(indegree[neighbour] == 0) q.push(neighbour);
+
+            // if it has indegree 0 then push into the queue
+            if(indegree[neighbour] == 0) 
+                q.push(neighbour);
         }
     }
-    return topo.size();
 }
 
 int main(){
@@ -43,18 +53,14 @@ int main(){
         graph[u].push_back(v);
     }
 
-    for(int i = 0; i < graph.size(); i++){
-        cout << i << " -> ";
-        for(int j = 0; j < graph[i].size(); j++){
-            cout<<graph[i][j]<<" ";
+    for(int ele = 0; ele < graph.size(); ele++){
+        cout << ele << " -> ";
+        for(auto neighbour : graph[ele]){
+            cout << neighbour << " ";
         }
-        cout<<"\n";
-    }
-    cout<<isCycle(graph)<<"\n";
-
-    // if there is difference in size then graph has a cycle as kahn's algo doesnot give correct answer for cyclic graphs
-    if(isCycle(graph) == graph.size()) cout << "The graph doesnot have cycle\n";
-    else cout << "The graph has a cycle\n";
+        cout << "\n";
+    }   
+    topologicalSort(graph);
 
     return 0;
 }
