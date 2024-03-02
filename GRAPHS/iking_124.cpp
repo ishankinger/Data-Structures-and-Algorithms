bf0f3aa9a93f1f460e9c7a48908658cc2c3beae0
ishan420 <116195006+ishankinger@users.noreply.github.com>
2024-03-02 15:17:27 +0530

    Update iking_124.cpp

--- GRAPHS/iking_124.cpp
+++ GRAPHS/iking_124.cpp
@@ -1,25 +1,38 @@
 // FIND EVENTUAL STATE USING BFS
+// EVENTUAL SAFE STATE QUEUSTION PREVIOUSLY DONE USING DFS( IF NODE IS ENTERING IN CYCLE OR PART OF IT THEN NOT A SAFE NODE)
+// NOW WE WILL DO THIS USING TOPOLOGICAL SORT
+// >> MAKE TRANSPOSE OF THE GIVEN GRAPH
+// >> THEN APPLY TOPOLOGICAL SORT
+// >> THE ELEMENTS WHICH ARE COMING IN TOPOLOGICAL SORT VECTOR ARE THE SAFE NODES OF THE GRAPH
 
 #include<bits/stdc++.h>
 using namespace std;
 
 int numOfSafeNodes(vector< vector<int> > graph){
+
+    // make one transpose graph in which arrow's direction is reversed
     vector< vector<int> > graph_transpose(graph.size(),vector<int>());
     for(int i = 0; i < graph.size(); i++){
         for(int j = 0; j < graph[i].size(); j++){
             graph_transpose[graph[i][j]].push_back(i);
         }
     }
+
+    // indegree vector for topo sort is formed
     vector<int> indegree(graph.size(),0);
     for(int i = 0; i < graph_transpose.size(); i++){
         for(int j = 0; j < graph_transpose[i].size(); j++){
             indegree[graph_transpose[i][j]]++;
         }
     }
+
+    // queue to have bfs traversal is also formed
     queue<int> q;
     for(int i = 0; i < indegree.size(); i++){
         if(indegree[i] == 0) q.push(i);
     }
+
+    // the elements which are coming in topo vector of the graph are the safe nodes
     vector<int> topo;
     while(not q.empty()){
         int ele = q.front();
@@ -31,10 +44,13 @@ int numOfSafeNodes(vector< vector<int> > graph){
                 q.push(graph_transpose[ele][i]);
         }
     }
+
+    // returning size of topological vector
     return topo.size();
 }
 
 int main(){
+
     int v;
     cin >> v;
     vector< vector<int> > graph(v,vector<int>());
@@ -55,5 +71,6 @@ int main(){
     }
 
     cout<<numOfSafeNodes(graph);
+    
     return 0;
 }
