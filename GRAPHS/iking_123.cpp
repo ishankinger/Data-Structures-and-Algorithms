50f256579b888ed216390696822480fbf2c17009
ishan420 <116195006+ishankinger@users.noreply.github.com>
2024-03-02 15:15:45 +0530

    Update iking_123.cpp

--- GRAPHS/iking_123.cpp
+++ GRAPHS/iking_123.cpp
@@ -1,8 +1,13 @@
 // COURSE SCHEDULE
+// N TASKS ARE GIVEN FROM 0 TO N-1 AND P PRE-REQUISITES ARE ALSO GIVEN [M,N]-> MEANS TO COMPLETE Mth TASK WE NEED TO FIRST COMPLETE TASK Nth
+// SO WE NEED TO TELL THAT ALL PRE-REQUISITES SATISFIED TO ANY COURSE SCHEDULE OR NOT
+// SO WE WILL ASSUME PRE-REQUISITE AS M ----> N GRAPH EDGE AND RUN TOPO SORT ON IT
+// IF TOPO SORT DOESNOT DETECT THE CYCLE THEN RETURN TRUE THAT SCHEDULE CAN BE FORMED OTHERWISE SCHEDULE CAN'T BE FORMED
 
 #include<bits/stdc++.h>
 using namespace std;
 
+// function to check whether course can be formed or not
 vector<int> courseSchedule(vector< vector<int> > graph){
     vector<int> indegree(graph.size(),0);
     for(int i = 0; i < graph.size(); i++){
@@ -23,6 +28,7 @@ vector<int> courseSchedule(vector< vector<int> > graph){
             if((--indegree[neighbour]) == 0) q.push(neighbour);
         }
     }
+    // if size of topo vector equal to graph size means no cycle is present thus return topo vector as final ans
     if(topo.size() == graph.size()) return topo;
 }
 
